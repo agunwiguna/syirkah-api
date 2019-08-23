098397407ce94833de1594d18c506908a0539559
agunwiguna <wgnagun@gmail.com>
2019-08-23 09:56:16 +0700

    ubah koneksi database

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
 
-$koneksi = mysqli_connect('localhost','root','','syirkah');
+$koneksi = mysqli_connect('localhost','solutiondipointe_syirkah','9smRJXXsc_9I','solutiondipointe_syirkah');
 
 ?>
