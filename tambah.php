4a68bb73dffeef8d1e764021216eb76ecfa56bbb
ciamiscode <ciamiscode@gmail.com>
2019-08-26 10:47:07 +0700

    ujicoba tambah.php

---
+++ tambah.php
@@ -0,0 +1,16 @@
+<?php
+require_once './koneksi/koneksi.php';
+
+ if($_SERVER['REQUEST_METHOD'] == 'POST')
+ {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $query = "INSERT INTO akun (email, password) VALUES ('$email','$password')";
+  $excQuery = mysqli_query($con, $query); 
+  echo ($excQuery) ? json_encode(array('status_code' =>200, 'message' => 'berhasil menambahkan data '.$nama)) :  json_encode(array('status_code' =>202, 'message' => 'data gagal ditambahkan'));
+ }
+ else
+ {
+   echo json_encode(array('status_code' =>404, 'message' => 'request tidak valid'));
+ }
+?>
