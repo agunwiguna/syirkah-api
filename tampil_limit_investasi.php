58f1702a28961f8b27e68bf80e88e3835b001ce8
agunwiguna <wgnagun@gmail.com>
2019-08-29 09:04:40 +0700

    ubah limit investasi

--- tampil_limit_investasi.php
+++ tampil_limit_investasi.php
@@ -6,10 +6,10 @@ if($_SERVER['REQUEST_METHOD']=='GET') {
   $sql = "SELECT * FROM investasi a JOIN users b ON a.id_user=b.id_user ORDER BY id_investasi DESC LIMIT 3 ";
   $res = mysqli_query($koneksi,$sql);
   $count = mysqli_num_rows($res);
-  $result_all_investasi = array();
+  $result_limit_investasi = array();
   if ($count>0) {
   	while($row = mysqli_fetch_array($res)){
-  		array_push($result_all_investasi, array(
+  		array_push($result_limit_investasi, array(
   			'id_investasi'=>$row[0],
   			'id_user'=>$row[1],
   			'nama_investasi'=>$row[2], 
@@ -20,7 +20,7 @@ if($_SERVER['REQUEST_METHOD']=='GET') {
   			'total_biaya'=>$row[7])
   	);
   	}
-  	echo json_encode(array("status_code"=>200,"result_all_investasi"=>$result_all_investasi));
+  	echo json_encode(array("status_code"=>200,"result_limit_investasi"=>$result_limit_investasi));
   	mysqli_close($koneksi);
   } else {
   	echo json_encode(array('status_code' =>404, 'message' => 'Upps.. data masih kosong'));
