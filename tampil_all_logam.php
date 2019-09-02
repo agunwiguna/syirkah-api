586ebe9537213351d60d507330c3ea8443e27e03
agunwiguna <ciamiscode@gmail.com>
2019-09-02 15:41:19 +0700

    add all logam.php

---
+++ tampil_all_logam.php
@@ -0,0 +1,40 @@
+<?php
+require_once('./koneksi/koneksi.php');
+
+if($_SERVER['REQUEST_METHOD']=='GET') {
+
+  $sql = "SELECT * FROM logam_mulia a JOIN users b ON a.id_user=b.id_user WHERE a.status='Belum Terjual' ORDER BY id_logam DESC";
+  
+  $res = mysqli_query($koneksi,$sql);
+  $count = mysqli_num_rows($res);
+  $result_logam = array();
+  if ($count>0) {
+    while($row = mysqli_fetch_array($res)){
+      array_push($result_logam, array(
+        'id_logam'=>$row[0],
+        'id_user'=>$row[1],
+        'jenis'=>$row[2], 
+        'berat'=>$row[3], 
+        'tahun_produksi'=>$row[4],
+        'harga_beli'=>$row[5],
+        'harga_jual'=>$row[6],
+        'status'=>$row[7],
+        'id_user'=>$row[8],
+        'nama'=>$row[9],
+        'alamat'=>$row[10],
+        'email'=>$row[11],
+        'telpon'=>$row[12],
+        'perusahaan'=>$row[13],
+        'alamat_perusahaan'=>$row[14],
+        'password'=>$row[15],
+        'foto'=>$row[16])
+      );
+    }
+    echo json_encode(array("status_code"=>200,"result_logam"=>$result_logam));
+    mysqli_close($koneksi);
+  } else {
+    echo json_encode(array('status_code' =>404, 'message' => 'Upps.. Data Jual Logam masih kosong'));
+  }
+}
+
+?>
