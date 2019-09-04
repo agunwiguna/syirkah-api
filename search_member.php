0930619a6a862d2dc4216b3d33bede2c82f973fd
agunwiguna <ciamiscode@gmail.com>
2019-09-04 20:31:24 +0700

    add search

---
+++ search_member.php
@@ -0,0 +1,36 @@
+<?php  
+
+require_once('./koneksi/koneksi.php');
+
+if($_SERVER['REQUEST_METHOD']=='POST') {
+
+  $search = $_POST['search'];
+
+  $sql = "SELECT * FROM users WHERE nama LIKE '%$search%'";
+  $res = mysqli_query($koneksi,$sql);
+  $count = mysqli_num_rows($res);
+  $result_member = array();
+  if ($count>0) {
+  	while($row = mysqli_fetch_array($res)){
+  		array_push($result_member, array(
+  			'id_user'=>$row[0],
+  			'nama'=>$row[1],
+  			'alamat'=>$row[2], 
+  			'email'=>$row[3], 
+  			'telpon'=>$row[4],
+  			'perusahaan'=>$row[5],
+  			'alamat_perusahaan'=>$row[6],
+  			'password'=>$row[7],
+  			'foto'=>$row[8],
+  			'emas'=>$row[9],
+        'perak'=>$row[10])
+  	);
+  	}
+  	echo json_encode(array("status_code"=>200,"result_member"=>$result_member));
+  	mysqli_close($koneksi);
+  } else {
+  	echo json_encode(array('status_code' =>404, 'message' => 'Upps.. data tidak ditemukan'));
+  }
+}
+
+?>
